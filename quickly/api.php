1bfce224b433529fb5ff1f5549b81b55df0e374e
root <root@vm94627.local>
2015-03-31 09:32:53 +0300

    Add method getCategories, getFoods, getDopFoods

--- quickly/api.php
+++ quickly/api.php
@@ -17,7 +17,21 @@ if (!empty($_POST)){
 		case 'getAllRestaurants':
 			getAllRestaurants();
 			break;
-		
+		case 'getCategories':
+			if(!empty($_POST['restaurant_id'])){
+				getCategories($_POST['restaurant_id']);
+			}
+			break;
+		case 'getFoods':
+			if(!empty($_POST['restaurant_id']) && !empty($_POST['categorie_id'])){
+				getFoods($_POST['restaurant_id'],$_POST['categorie_id']);
+			}
+			break;
+		case 'getDopFoods':
+			if(!empty($_POST['restaurant_id']) && !empty($_POST['categorie_id']) && !empty($_POST['food_id'])){
+				getDopFoods($_POST['restaurant_id'],$_POST['categorie_id'],$_POST['food_id']);
+			}
+			break;
 		default:
 			break;
 	}
@@ -57,6 +71,38 @@ function getAllRestaurants(){
 
 }
 
+function getCategories($restaurant_id){
+	$result = mysql_query("SELECT * from categories WHERE restaurant_id =".$restaurant_id);
+	$rows = array();
+	while ($r = mysql_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+
+	echo json_encode($rows);
+}
+
+function getFoods($categorie_id, $restaurant_id){
+	$result = mysql_query("SELECT * from foods WHERE restaurant_id =".$restaurant_id."
+							AND priority_meal = 0 AND categorie_id =".$categorie_id);
+	$rows = array();
+	while ($r = mysql_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+
+	echo json_encode($rows);	
+}		
+
+function getDopFoods($categorie_id, $restaurant_id, $food_id){
+	$result = mysql_query("SELECT * from foods WHERE restaurant_id =".$restaurant_id."
+							AND priority_meal = 1 AND categorie_id =".$categorie_id."
+							AND id =".$food_id);
+	$rows = array();
+	while ($r = mysql_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+
+	echo json_encode($rows);	
+}		
 
 db_disconnect($db_link);
 ?>
